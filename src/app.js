d5d9e87e4e4e9ac1d6afd247c403c965cd5ed450
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-06-22 14:52:05 +0300

    API connected

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 function showTemp(response) {
   console.log(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let tempElement = document.querySelector(".temp");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "6243bd378295e87dcd4f90e3e23db829";
