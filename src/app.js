d15932ca1d35e6e506d42637bff8bdbd8ced1277
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-06-22 14:40:32 +0300

    added api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function showTemp(response) {
+  console.log(response.data.main.temp);
+}
 
+let apiKey = "6243bd378295e87dcd4f90e3e23db829";
+let city = "Zvenyhorod";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemp);
