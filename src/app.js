37a678ab25a2236664312c70f29313e757df3617
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-06-22 15:52:04 +0300

    current temperature show

--- src/app.js
+++ src/app.js
@@ -4,8 +4,14 @@ function showTemp(response) {
   cityElement.innerHTML = response.data.name;
   let tempElement = document.querySelector(".temp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+  let feelElement = document.querySelector("#feels-like");
+  feelElement.innerHTML = Math.round(response.data.main.feels_like);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "6243bd378295e87dcd4f90e3e23db829";
