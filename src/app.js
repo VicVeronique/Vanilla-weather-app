8ee4c3d695ffef1e3490fdf210599ed8196ff472
VicVeronique <107785778+VicVeronique@users.noreply.github.com>
2022-06-28 13:15:26 +0300

    unit conversion for forecast

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// CURRENT DATE AND TIME DISPLAY
+
 function formatDay(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -8,7 +10,6 @@ function formatDay(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
   let days = [
     "Sunday",
     "Monday",
@@ -44,6 +45,8 @@ function formatDate(timestamp) {
   return `${month} ${dateDay}, ${year}`;
 }
 
+// FORECAST DAYS DISPLAY
+
 function formatForecastDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -52,7 +55,7 @@ function formatForecastDay(timestamp) {
 }
 
 function showForecast(response) {
-  let forecast = response.data.daily;
+  forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
@@ -85,33 +88,42 @@ function showForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+// API FOR FORECAST
+
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "6243bd378295e87dcd4f90e3e23db829";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(showForecast);
 }
 
-function showTemp(response) {
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
+// DISPLAY OF CURRENT WEATHER
 
+function showTemp(response) {
   celsiusTemp = response.data.main.temp;
 
   let tempElement = document.querySelector(".temp");
   tempElement.innerHTML = Math.round(response.data.main.temp);
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
+
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
+
   let dayElement = document.querySelector("#day-time");
   dayElement.innerHTML = formatDay(response.data.dt * 1000);
+
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
@@ -122,6 +134,8 @@ function showTemp(response) {
   getForecast(response.data.coord);
 }
 
+// API FOR CURRENT WEATHER
+
 function search(city) {
   let apiKey = "6243bd378295e87dcd4f90e3e23db829";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -129,11 +143,15 @@ function search(city) {
   axios.get(apiUrl).then(showTemp);
 }
 
+// SEARCH A CITY
+
 function searchSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+
+// WEATHER TEMPERATURE CONVERT
 
 function fahrenheitChange(event) {
   event.preventDefault();
@@ -142,6 +160,37 @@ function fahrenheitChange(event) {
   // replace class
   celsiusLink.classList.replace("active", "passive");
   fahrenheitLink.classList.replace("passive", "active");
+
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `           
+                <div class="col-2">
+                  <div class="forecast-wrap">
+                  <div class="date">${formatForecastDay(forecastDay.dt)}</div>
+                  <img src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" alt="" width="42">
+                  <div class="temperature-forecast">
+                    <span class="max-temp-forecast">${Math.round(
+                      (forecastDay.temp.max * 9) / 5 + 32
+                    )}°</span>
+                    <span class="min-temp-forecast">${Math.round(
+                      (forecastDay.temp.min * 9) / 5 + 32
+                    )}°</span>
+                  </div>
+                  </div>
+                </div>           
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function celsiusChange(event) {
@@ -151,14 +200,38 @@ function celsiusChange(event) {
   // replace class
   celsiusLink.classList.replace("passive", "active");
   fahrenheitLink.classList.replace("active", "passive");
+
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `           
+                <div class="col-2">
+                  <div class="forecast-wrap">
+                  <div class="date">${formatForecastDay(forecastDay.dt)}</div>
+                  <img src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png" alt="" width="42">
+                  <div class="temperature-forecast">
+                    <span class="max-temp-forecast">${Math.round(
+                      forecastDay.temp.max
+                    )}°</span>
+                    <span class="min-temp-forecast">${Math.round(
+                      forecastDay.temp.min
+                    )}°</span>
+                  </div>
+                  </div>
+                </div>           
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
-
-let celsiusTemp = null;
-
-search("Zvenyhorod");
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", fahrenheitChange);
@@ -166,7 +239,18 @@ fahrenheitLink.addEventListener("click", fahrenheitChange);
 let celsiusLink = document.querySelector("#celcius-link");
 celsiusLink.addEventListener("click", celsiusChange);
 
-//example cities
+//
+
+let celsiusTemp = null;
+let forecast = null;
+
+search("Zvenyhorod");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchSubmit);
+
+// EXAMPLE CITIES CURRENT WEATHER AND FORECAST SEARTH
+
 let newYorkcity = document.querySelector("#new-york-city");
 newYorkcity.addEventListener("click", newYorkSubmit);
 
